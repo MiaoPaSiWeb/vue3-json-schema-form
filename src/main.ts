ee7851283e11da0edc94c7ac2e8e72557d8915d6
yuhui.Mr <zhuyuhui434@gmail.com>
2022-04-17 12:24:40 +0800

    2 7 setup返回render函数的用法

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp, defineComponent, h } from "vue";
-import App from "./App.vue";
-// import HelloWorld from "./components/HelloWorld.vue";
+import { createApp, defineComponent, h, reactive } from "vue";
+// import App from "./App.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 // import img from "./assets/logo.png"; //这种ts校验不通过
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const img = require("./assets/logo.png");
+const img = require("./assets/logo.png");
 /*
 <template>
   <div>
@@ -15,22 +15,28 @@ import App from "./App.vue";
 </template>
 */
 
-// const App = defineComponent({
-//   render() {
-//     return h("div", { id: "app" }, [
-//       /*
-//       Q:为什么这里直接src: "./assets/logo.png"，图片会出不来？
-//       在.vue文件中的template中使用相对路径，vue-loader会进行寻址，所有能正确加载图片
-//       这里应该更改引用方式：
-//         const img = require("./assets/logo.png");
-//       */
-//       h("img", { alt: "Vue logo", src: img }),
-//       h(HelloWorld, {
-//         msg: "Welcome to Your Vue.js + TypeScript App",
-//         age: 31,
-//       }),
-//     ]);
-//   },
-// });
+const App = defineComponent({
+  // setup 函数只会执行一次
+  setup() {
+    const stateRef = reactive({
+      name: "PjssName",
+    });
+
+    setInterval(() => {
+      stateRef.name += "1";
+    }, 1000);
+
+    return () => {
+      return h("div", { id: "app" }, [
+        h("img", { alt: "Vue logo", src: img }, []),
+        h("p", stateRef.name),
+        h(HelloWorld, {
+          msg: "Welcome to Your",
+          age: 12,
+        }),
+      ]);
+    };
+  },
+});
 
 createApp(App).mount("#app");
