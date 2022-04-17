3da3fc12530a052d0905dc3618b26a464d5516ba
yuhui.Mr <zhuyuhui434@gmail.com>
2022-04-17 11:30:45 +0800

    2 6 setup的运用和其意义

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,39 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" :age="12" />
+    <p>countRef: {{ countRef }}</p>
+    <p>nameRef : {{ nameRef }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent, ref, watchEffect } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     HelloWorld,
+  },
+  setup() {
+    // ref实际返回的是一个 { value:xxx } 的结构对象，即每个ref都有一个value属性
+    const countRef = ref(0);
+
+    // computed
+    const nameRef = computed(() => {
+      return "Hello World" + countRef.value;
+    });
+
+    // watchEffect（监听器）
+    watchEffect(() => {
+      console.log(nameRef.value);
+    });
+
+    // setInterval(() => {
+    //   countRef.value += 1;
+    // }, 1000);
+
+    return { countRef, nameRef };
   },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp, defineComponent, h } from "vue";
-// import App from "./App.vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import App from "./App.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 
 // import img from "./assets/logo.png"; //这种ts校验不通过
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const img = require("./assets/logo.png");
+// const img = require("./assets/logo.png");
 /*
 <template>
   <div>
@@ -15,22 +15,22 @@ const img = require("./assets/logo.png");
 </template>
 */
 
-const App = defineComponent({
-  render() {
-    return h("div", { id: "app" }, [
-      /*
-      Q:为什么这里直接src: "./assets/logo.png"，图片会出不来？
-      在.vue文件中的template中使用相对路径，vue-loader会进行寻址，所有能正确加载图片
-      这里应该更改引用方式：
-        const img = require("./assets/logo.png");
-      */
-      h("img", { alt: "Vue logo", src: img }),
-      h(HelloWorld, {
-        msg: "Welcome to Your Vue.js + TypeScript App",
-        age: 31,
-      }),
-    ]);
-  },
-});
+// const App = defineComponent({
+//   render() {
+//     return h("div", { id: "app" }, [
+//       /*
+//       Q:为什么这里直接src: "./assets/logo.png"，图片会出不来？
+//       在.vue文件中的template中使用相对路径，vue-loader会进行寻址，所有能正确加载图片
+//       这里应该更改引用方式：
+//         const img = require("./assets/logo.png");
+//       */
+//       h("img", { alt: "Vue logo", src: img }),
+//       h(HelloWorld, {
+//         msg: "Welcome to Your Vue.js + TypeScript App",
+//         age: 31,
+//       }),
+//     ]);
+//   },
+// });
 
 createApp(App).mount("#app");
