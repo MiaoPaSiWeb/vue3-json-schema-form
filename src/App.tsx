bf96a53e5433ee2546b9adc13254d2e27d913d76
yuhui.Mr <zhuyuhui434@gmail.com>
2022-04-18 15:14:19 +0800

    4 2 实现demo项目
    安装插件：
     "jss": "^10.9.0",
    "jss-preset-default": "^10.9.0",
    "vue": "^3.2.13",
    "vue-jss": "^0.0.4"

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,205 @@
 import {
-  computed,
+  createApp,
   defineComponent,
+  h,
   reactive,
   ref,
+  Ref,
   watchEffect,
-  withModifiers,
 } from "vue";
 
-import HelloWorld from "./components/HelloWorld.vue";
+import { createUseStyles } from "vue-jss";
 
-const img = require("./assets/logo.png"); // eslint-disable-line
+import MonacoEditor from "./components/MonacoEditor";
+import demos from "./demos";
+import SchemaForm from "../lib";
 
-function renderHelloWorld(num: number) {
-  return <HelloWorld age={num} />;
+type Schema = any;
+type UISchema = any;
+
+function toJson(data: any) {
+  return JSON.stringify(data, null, 2); // 使用两个空格缩进
 }
+
+// js编写css样式
+const useStyles = createUseStyles({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    width: "1200px",
+    margin: "0 auto",
+  },
+  menu: {
+    marginBottom: 20,
+  },
+  code: {
+    width: 700,
+    flexShrink: 0,
+  },
+  codePanel: {
+    minHeight: 400,
+    marginBottom: 20,
+  },
+  uiAndValue: {
+    display: "flex",
+    justifyContent: "space-between",
+    "& > *": {
+      width: "46%",
+    },
+  },
+  content: {
+    display: "flex",
+  },
+  form: {
+    padding: "0 20px",
+    flexGrow: 1,
+  },
+  menuButton: {
+    appearance: "none",
+    borderWidth: 0,
+    backgroundColor: "transparent",
+    cursor: "pointer",
+    display: "inline-block",
+    padding: 15,
+    borderRadius: 5,
+    "&:hover": {
+      background: "#efefef",
+    },
+  },
+  menuSelected: {
+    background: "#337ab7",
+    color: "#fff",
+    "&:hover": {
+      background: "#337ab7",
+    },
+  },
+});
+
+const schema = {
+  type: "string",
+};
 
 export default defineComponent({
   setup() {
-    const state = reactive({
-      name: "Helow",
-      message: "",
+    const selectedRef: Ref<number> = ref(0);
+
+    const demo: {
+      schema: Schema | null;
+      data: any;
+      uiSchema: UISchema | null;
+      schemaCode: string;
+      dataCode: string;
+      uiSchemaCode: string;
+    } = reactive({
+      schema: null,
+      data: {},
+      uiSchema: {},
+      schemaCode: "",
+      dataCode: "",
+      uiSchemaCode: "",
     });
 
-    const countRef = ref(0);
+    watchEffect(() => {
+      const index = selectedRef.value;
+      const d = demos[index];
+      demo.schema = d.schema;
+      demo.data = d.default;
+      demo.uiSchema = d.uiSchema;
+      demo.schemaCode = toJson(d.schema);
+      demo.dataCode = toJson(d.default);
+      demo.uiSchemaCode = toJson(d.uiSchema);
+    });
 
-    setInterval(() => {
-      state.name += "1";
-      countRef.value += 1;
-    }, 1000);
+    const methodRef: Ref<any> = ref();
+
+    const classesRef = useStyles();
+
+    const handleChange = (v: any) => {
+      demo.data = v;
+      demo.dataCode = toJson(v);
+    };
+
+    function handleCodeChange(
+      filed: "schema" | "data" | "uiSchema",
+      value: string,
+    ) {
+      try {
+        const json = JSON.parse(value);
+        demo[filed] = json;
+        (demo as any)[`${filed}Code`] = value;
+      } catch (err) {
+        // some thing
+      }
+    }
+
+    const handleSchemaChange = (v: string) => handleCodeChange("schema", v);
+    const handleDataChange = (v: string) => handleCodeChange("data", v);
+    const handleUISchemaChange = (v: string) => handleCodeChange("uiSchema", v);
 
     return () => {
-      const count = countRef.value;
+      const classes = classesRef.value;
+      const selected = selectedRef.value;
 
-      console.log(state.message + "--------");
+      console.log(methodRef);
+
       return (
-        <div id="app">
-          <img alt="Vue logo" src={img} />
-          <p>{state.name + count}</p>
-          <input type="text" v-model={state.message} />
-          {renderHelloWorld(13)}
+        // <StyleThemeProvider>
+        // <VJSFThemeProvider theme={theme as any}>
+        <div class={classes.container}>
+          <div class={classes.menu}>
+            <h1>Vue3 JsonSchema Form</h1>
+            <div>
+              {demos.map((demo, index) => (
+                <button
+                  class={{
+                    [classes.menuButton]: true,
+                    [classes.menuSelected]: index === selected,
+                  }}
+                  onClick={() => (selectedRef.value = index)}
+                >
+                  {demo.name}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div class={classes.content}>
+            <div class={classes.code}>
+              <MonacoEditor
+                code={demo.schemaCode}
+                class={classes.codePanel}
+                onChange={handleSchemaChange}
+                title="Schema"
+              />
+              <div class={classes.uiAndValue}>
+                <MonacoEditor
+                  code={demo.uiSchemaCode}
+                  class={classes.codePanel}
+                  onChange={handleUISchemaChange}
+                  title="UISchema"
+                />
+                <MonacoEditor
+                  code={demo.dataCode}
+                  class={classes.codePanel}
+                  onChange={handleDataChange}
+                  title="Value"
+                />
+              </div>
+            </div>
+            <div class={classes.form}>
+              <SchemaForm />
+              {/* <SchemaForm
+                schema={demo.schema!}
+                uiSchema={demo.uiSchema!}
+                onChange={handleChange}
+                contextRef={methodRef}
+                value={demo.data}
+              /> */}
+            </div>
+          </div>
         </div>
+        // </VJSFThemeProvider>
+        // </StyleThemeProvider>
       );
     };
   },
