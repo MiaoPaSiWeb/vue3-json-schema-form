ab00c8210199fe1314561b8ebaeee321c9c3b143
yuhui.Mr <zhuyuhui434@gmail.com>
2022-04-18 12:17:26 +0800

    3 2 如何试用ajv来定义和校验json schema

---
+++ schema-tests/test1.js
@@ -0,0 +1,22 @@
+const Ajv = require("ajv");
+const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+
+const schema = {
+  type: "object",
+  properties: {
+    foo: { type: "integer" },
+    bar: { type: "string" },
+  },
+  required: ["foo"],
+  additionalProperties: false,
+};
+
+const validate = ajv.compile(schema);
+
+const data = {
+  //   foo: 1,
+  bar: "abc",
+};
+
+const valid = validate(data);
+if (!valid) console.log(validate.errors);
