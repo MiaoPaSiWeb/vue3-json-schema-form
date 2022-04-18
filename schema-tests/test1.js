e154430ec178fd7e829f3a5f1c651a2fd50af8bc
yuhui.Mr <zhuyuhui434@gmail.com>
2022-04-18 12:49:06 +0800

    3 3 json schema的fomart和自定义format

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -1,21 +1,39 @@
 const Ajv = require("ajv");
+const addFormats = require("ajv-formats");
 const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+
+// Error: unknown format “email” ignored in schema at path "#/properties/name"
+// 想在当前的文件下使用format你就要把format引入进来，
+// 你如果不引入就使用这个方法，文件是无法找到这个方法的，就会报这个错误。
+// 解决方法是在使用这个方法的文件下，引入format。
+addFormats(ajv);
 
 const schema = {
   type: "object",
   properties: {
     foo: { type: "integer" },
-    bar: { type: "string" },
+    bar: { type: "string", format: "email" },
+    age: {
+      type: "number",
+      format: "qy", //****自定义***
+    },
   },
-  required: ["foo"],
-  additionalProperties: false,
 };
+
+//自定义
+ajv.addFormat("qy", {
+  type: "number", //输入类型必须是number
+  validate: (x) => {
+    return x > 5 ? true : false;
+  },
+});
 
 const validate = ajv.compile(schema);
 
 const data = {
-  //   foo: 1,
-  bar: "abc",
+  foo: 1,
+  bar: "abc@xxx.com",
+  age: 0,
 };
 
 const valid = validate(data);
